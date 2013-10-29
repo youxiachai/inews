21b216cd5113eab6022982875e15a644c81c09bb
hfcorriez <hfcorriez@gmail.com>
2013-10-30 01:35:52 +0800

    首页增加最新的几篇文章

--- app/src/Route/Web/Index.php
+++ app/src/Route/Web/Index.php
@@ -19,6 +19,26 @@ class Index extends Web
         $total = Model::factory('Article')->where('status', '1')->count();
 
         $this->data['articles'] = Model::factory('Article')->where('status', '1')->order_by_desc('point')->offset($offset)->limit($limit)->find_many();
+        $this->data['latest_articles'] = array();
+
+        /**
+         * 第一页加载最新的几篇文章
+         */
+        if ($page == 1) {
+            $this->data['latest_articles'] = Model::factory('Article')->where('status', '1')->order_by_desc('created_at')->limit(5)->find_many();
+
+            if ($this->data['latest_articles'] && !$this->input->query('force_latest')) {
+                $exists_articles_ids = array();
+                foreach ($this->data['articles'] as $article) {
+                    $exists_articles_ids[] = $article->id;
+                }
+
+                $this->data['latest_articles'] = array_filter($this->data['latest_articles'], function ($article) use ($exists_articles_ids) {
+                    if (in_array($article->id, $exists_articles_ids)) return false;
+                    return true;
+                });
+            }
+        }
 
         $this->data['page'] = Html::makePage($this->input->uri(), 'page=(:num)', $page, $total, $limit);
     }
