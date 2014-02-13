5d58a7efb82c0807ddd6fae772a77dda4b9c4897
youxiachai <youxiachai@gmail.com>
2014-02-13 17:06:14 +0800

    init project

--- app/views/article/delete.php
+++
@@ -1,6 +0,0 @@
-<div class="typo wrapper-padding">
-    <form action="" method="post">
-        Do u really want to delete the article?
-        <input class="btn" type="submit" value="Yes" />
-    </form>
-</div>
--- app/views/embed.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$embed = \Helper\Url::parseEmbed($article->link);
-
-if (!$embed) return;
-
-?>
-<div class="typo-p"><?php echo $embed['html']; ?></div>
