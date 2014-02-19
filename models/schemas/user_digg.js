be48a30b1a591ed78c2f68e2547524b098136cdf
youxiachai <youxiachai@gmail.com>
2014-02-19 16:50:41 +0800

    fix sqlite3 unique index

--- models/schemas/user_digg.js
+++ models/schemas/user_digg.js
@@ -4,8 +4,8 @@
 module.exports = function (sequelize, DataType) {
     return sequelize.define('user_digg', {
         id: {type: DataType.INTEGER, primaryKey: true, autoIncrement: true},
-        user_id : {type :  DataType.INTEGER, unique : true},
-        article_id : {type :  DataType.INTEGER, unique: true}
+        user_id : {type :  DataType.INTEGER},
+        article_id : {type :  DataType.INTEGER}
     },{
         timestamps : true,
         'createdAt' : 'created_at',
